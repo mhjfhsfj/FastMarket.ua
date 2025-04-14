43d4f64caf56ef069824a63a301606e4ed59e95f
Mykola Bidnenko <bidnenkomykola91@gmail.com>
2025-04-14 19:14:47 +0300

    готове для тестової розгортки

--- FastMarketBackEnd/FastMarketBackEnd/obj/Debug/net8.0/FastMarketBackEnd.AssemblyInfo.cs
+++ FastMarketBackEnd/FastMarketBackEnd/obj/Debug/net8.0/FastMarketBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastMarketBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d476762e151b2cb7009f1492cec4cb069578df6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a283ee9ba18cd9706890a60316fd7218a6e90a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastMarketBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastMarketBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
